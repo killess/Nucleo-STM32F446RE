a1c15dbb42b6354279d43776b7626ff7ef650517
Greg Hughes <ghughes1020@gmail.com>
2018-11-26 17:35:33 -0500

    renamed file

--- Source/interrupts.c
+++ Source/interrupts.c
@@ -4,13 +4,13 @@
 
 
 
-// brief This function handles Non maskable interrupt.
+// This function handles Non maskable interrupt.
 void NMI_Handler(void)
 {
 }
 
 
-// brief This function handles Hard fault interrupt.
+// This function handles Hard fault interrupt.
 void HardFault_Handler(void)
 {
   while (1)
@@ -19,7 +19,7 @@ void HardFault_Handler(void)
 }
 
 
-// brief This function handles Memory management fault.
+// This function handles Memory management fault.
 void MemManage_Handler(void)
 {
   while (1)
@@ -28,7 +28,7 @@ void MemManage_Handler(void)
 }
 
 
-// brief This function handles Pre-fetch fault, memory access fault.
+// This function handles Pre-fetch fault, memory access fault.
 void BusFault_Handler(void)
 {
   while (1)
@@ -37,7 +37,7 @@ void BusFault_Handler(void)
 }
 
 
-// brief This function handles Undefined instruction or illegal state.
+// This function handles Undefined instruction or illegal state.
 void UsageFault_Handler(void)
 {
   while (1)
@@ -46,25 +46,25 @@ void UsageFault_Handler(void)
 }
 
 
-// brief This function handles System service call via SWI instruction.
+// This function handles System service call via SWI instruction.
 void SVC_Handler(void)
 {
 }
 
 
-// brief This function handles Debug monitor.
+// This function handles Debug monitor.
 void DebugMon_Handler(void)
 {
 }
 
 
-// brief This function handles Pendable request for system service.
+// This function handles Pendable request for system service.
 void PendSV_Handler(void)
 {
 }
 
 
-// brief This function handles System tick timer.
+// This function handles System tick timer.
 void SysTick_Handler(void)
 {
   HAL_IncTick();
